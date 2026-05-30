cff1e8fef1b8923a099dcc1e9ab7604090ec0976
kudston <kadiabdulkudus@gmail.com>
2026-05-30 15:37:32 +0000

    added min_target_points to base processor

--- src/pipeline/trainer.py
+++ src/pipeline/trainer.py
@@ -314,7 +314,7 @@ class Trainer:
                                           sequence_length, self.config, model_id,
                                           stride=self.config.test_generator_stride, 
                                           eval_metrics=_reason_map,
-                                          min_target_points=min_target_points,
+                                          min_target_points=min_target_point,
                                           )
         except Exception as error:
             LOGGER.warning("Optional fg-tester call failed for %s/%s: %s", symbol, model_type, error)
