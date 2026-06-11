5c362bf17c8090e2b8230e11cfbde5d00f55e28d
Abdulkudus <98144805+Kudston@users.noreply.github.com>
2026-06-11 22:30:35 +0100

    Update generate_train_data.py

--- src/pipeline/generate_train_data.py
+++ src/pipeline/generate_train_data.py
@@ -7,6 +7,8 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(1)
 import keras
 import os
 
